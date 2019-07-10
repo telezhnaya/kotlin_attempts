cf731167638a0de8e9af238fa7a75238231b8d70
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2019-07-10 08:50:42 +0300

    add gradle

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,6 +1,8 @@
 import java.nio.file.Paths
+import org.apache.commons.net.ftp.FTPClient
 
 fun main() {
+
     // TODO threads!!!
     val app = MainWindow(Paths.get("").toAbsolutePath().toFile())
     app.isVisible = true
