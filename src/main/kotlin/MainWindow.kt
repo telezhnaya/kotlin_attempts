95890104f32db4e850c31fa083d007d00f2ceda0
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2019-07-13 17:40:11 +0300

    add new UI features (curPath, settings)

--- src/main/kotlin/MainWindow.kt
+++ src/main/kotlin/MainWindow.kt
@@ -1,8 +1,5 @@
 import observer.IFileList
-import java.awt.Component
-import java.awt.Container
-import java.awt.GridLayout
-import java.awt.Toolkit
+import java.awt.*
 import java.awt.event.KeyAdapter
 import java.awt.event.KeyEvent
 import java.awt.event.MouseAdapter
@@ -11,92 +8,117 @@ import javax.swing.*
 
 
 class MainWindow(private var fileList: IFileList) : JFrame("Best file manager ever (or not)") {
-    private val mainForm: Container
+    private val currentPath: JLabel
 
-    private val left: JList<String>
-    private val leftData = DefaultListModel<String>()
-
-    private var right: Component
+    private val pathsAndPreviewLayout = JPanel()
+    private val paths: JList<String>
+    private val pathsModel = DefaultListModel<String>()
+    private var preview: Component
 
     init {
         val screen = Toolkit.getDefaultToolkit().screenSize
         // window will be centered and take 2/3 of screen
         this.setBounds(screen.width / 6, screen.height / 6, screen.width / 3 * 2, screen.height / 3 * 2)
         this.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        val mainLayout = this.contentPane
+        val settingsLayout = JPanel()
 
-        mainForm = this.contentPane
-        mainForm.layout = GridLayout(1, 2)
+        mainLayout.layout = GridBagLayout()
+        settingsLayout.layout = GridBagLayout()
+        pathsAndPreviewLayout.layout = GridLayout(1, 2, 5, 0)
 
-//        // TODO ftp module, set local path manually
-//        val settings = JButton("Settings")
-//        settings.preferredSize = Dimension(10, 50)
-//        mainForm.add(settings)
+        currentPath = JLabel(fileList.getCurrentDir())
+        settingsLayout.add(currentPath, createGridBagConstraints(0, 0, 1.0, 0.0))
 
-        fillLeftForm(fileList.getPreview("").getFileList())
-        left = JList(leftData)
-        initLeftComponent()
-        mainForm.add(JScrollPane(left))
+        val settingsButton = JButton("Settings")
+        settingsLayout.add(settingsButton, createGridBagConstraints(1, 0, 0.0, 0.0))
+        mainLayout.add(settingsLayout, createGridBagConstraints(0, 0, 1.0, 0.0))
 
-        right = fileList.getPreview(leftData[0]).getDrawable(left.size) // TODO get rid of left here
-        mainForm.add(right)
+
+        fillPathsForm(fileList.getPreview("").getFileList())
+        paths = JList(pathsModel)
+        initPathsComponent()
+        pathsAndPreviewLayout.add(JScrollPane(paths))
+
+        preview = fileList.getPreview(pathsModel[0]).getDrawable(paths.size)
+        pathsAndPreviewLayout.add(preview)
+        mainLayout.add(pathsAndPreviewLayout, createGridBagConstraints(0, 1, 1.0, 1.0))
     }
 
-    private fun fillLeftForm(files: List<String>) {
-        leftData.removeAllElements()
-        leftData.add(0, "..")
-        leftData.addAll(files)
+    private fun createGridBagConstraints(gridx: Int, gridy: Int, weightx: Double, weighty: Double): GridBagConstraints {
+        return GridBagConstraints(
+            gridx,
+            gridy,
+            1,
+            1,
+            weightx,
+            weighty,
+            10,
+            GridBagConstraints.BOTH,
+            Insets(0, 0, 0, 0),
+            0,
+            0
+        )
     }
 
-    private fun initLeftComponent() {
-        left.selectionMode = ListSelectionModel.SINGLE_SELECTION
+    private fun fillPathsForm(files: List<String>) {
+        pathsModel.removeAllElements()
+        pathsModel.add(0, "..")
+        pathsModel.addAll(files)
+    }
 
-        left.addListSelectionListener {
-            if (left.selectedIndex != -1) {
-                right = fileList.getPreview(left.selectedValue).getDrawable(right.size)
-                reloadRightForm()
+    private fun initPathsComponent() {
+        paths.selectionMode = ListSelectionModel.SINGLE_SELECTION
+
+        paths.addListSelectionListener {
+            if (paths.selectedIndex != -1) {
+                preview = fileList.getPreview(paths.selectedValue).getDrawable(preview.size)
+                reloadPreviewForm()
             }
         }
 
-        left.addMouseListener(object : MouseAdapter() {
+        paths.addMouseListener(object : MouseAdapter() {
             override fun mouseClicked(evt: MouseEvent) {
                 if (evt.clickCount == 2) {
-                    openDirectory()
+                    openDirectoryPath()
                 }
             }
         })
 
-        left.addKeyListener(object : KeyAdapter() {
+        paths.addKeyListener(object : KeyAdapter() {
             override fun keyReleased(ke: KeyEvent) {
                 when (ke.keyCode) {
-                    KeyEvent.VK_RIGHT -> openDirectory()
-                    KeyEvent.VK_ENTER -> openDirectory()
+                    KeyEvent.VK_RIGHT -> openDirectoryPath()
+                    KeyEvent.VK_ENTER -> openDirectoryPath()
                     KeyEvent.VK_LEFT -> {
                         val cur = fileList.getCurrentDir()
                         fileList = fileList.goBack()
-                        fillLeftForm(fileList.getPreview("").getFileList())
-                        right = fileList.getPreview(cur).getDrawable(right.size)
-                        reloadRightForm()
+                        fillPathsForm(fileList.getPreview("").getFileList())
+                        preview = fileList.getPreview(cur).getDrawable(preview.size)
+                        reloadPreviewForm()
                     }
                 }
             }
         })
     }
 
-    private fun openDirectory() {
-        if (left.selectedValue == null) return
+    private fun openDirectoryPath() {
+        if (paths.selectedValue == null) return
 
-        fileList = fileList.goForward(left.selectedValue)
-        fillLeftForm(fileList.getPreview("").getFileList())
-        left.selectedIndex = 0
-        right = fileList.getPreview(leftData[0]).getDrawable(right.size)
-        reloadRightForm()
+        fileList = fileList.goForward(paths.selectedValue)
+        fillPathsForm(fileList.getPreview("").getFileList())
+        paths.selectedIndex = 0
+        preview = fileList.getPreview(pathsModel[0]).getDrawable(preview.size)
+        reloadPreviewForm()
+        currentPath.text = fileList.getCurrentDir()
+        currentPath.revalidate()
     }
 
-    private fun reloadRightForm() {
-        while (mainForm.componentCount > 1) {
-            mainForm.remove(1)
+    private fun reloadPreviewForm() {
+        while (pathsAndPreviewLayout.componentCount > 1) {
+            pathsAndPreviewLayout.remove(1)
         }
-        mainForm.add(right)
-        mainForm.revalidate()
+        pathsAndPreviewLayout.add(preview, 1)
+        pathsAndPreviewLayout.revalidate()
     }
 }
