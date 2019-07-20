f568664dcb4a615477538b06ed687b16d668f4e8
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2019-07-20 15:38:10 +0300

    Add anonymous ftp connection

--- src/main/kotlin/swing/window/FTPSettingsWindow.kt
+++ src/main/kotlin/swing/window/FTPSettingsWindow.kt
@@ -12,17 +12,25 @@ import swing.reloadText
 import java.awt.Dimension
 import java.awt.GridBagLayout
 import java.awt.GridLayout
-import java.awt.event.WindowAdapter
-import java.awt.event.WindowEvent
+import java.awt.event.*
 import java.net.ConnectException
 import javax.swing.*
 
 
-class FTPSettingsWindow(header: String, parent: JFrame) : JFrame(header) {
+class FTPSettingsWindow(header: String, private val parent: JFrame) : JFrame(header) {
+    private val address = JTextField(FTP_PREFIX)
+    private val anonymous = JCheckBox("Anonymous connection")
+
+    private val userLabel = JLabel("User")
+    private val user = JTextField()
+
+    private val passwordLabel = JLabel("Password")
+    private val password = JPasswordField()
+
     private val error = JTextArea()
 
     init {
-        this.size = Dimension(350, 200)
+        this.size = Dimension(350, 220)
         this.setLocationRelativeTo(null)
 
         val mainContainer = this.contentPane
@@ -31,17 +39,17 @@ class FTPSettingsWindow(header: String, parent: JFrame) : JFrame(header) {
         val settings = JPanel()
         settings.layout = GridBagLayout()
 
-        val address = JTextField(FTP_PREFIX)
         settings.add(JLabel("Server address"), createGridBagConstraints(0, 0, 0.0, 0.0))
         settings.add(address, createGridBagConstraints(1, 0, 1.0, 0.0))
 
-        val user = JTextField()
-        settings.add(JLabel("User"), createGridBagConstraints(0, 1, 0.0, 0.0))
-        settings.add(user, createGridBagConstraints(1, 1, 1.0, 0.0))
+        anonymous.addItemListener(AnonymousSelection(this))
+        settings.add(anonymous, createGridBagConstraints(0, 1, 1.0, 0.0, 2))
 
-        val password = JPasswordField()
-        settings.add(JLabel("Password"), createGridBagConstraints(0, 2, 0.0, 0.0))
-        settings.add(password, createGridBagConstraints(1, 2, 1.0, 0.0))
+        settings.add(userLabel, createGridBagConstraints(0, 2, 0.0, 0.0))
+        settings.add(user, createGridBagConstraints(1, 2, 1.0, 0.0))
+
+        settings.add(passwordLabel, createGridBagConstraints(0, 3, 0.0, 0.0))
+        settings.add(password, createGridBagConstraints(1, 3, 1.0, 0.0))
 
         mainContainer.add(settings, createGridBagConstraints(0, 0, 1.0, 1.0))
         error.initErrorField()
@@ -51,33 +59,70 @@ class FTPSettingsWindow(header: String, parent: JFrame) : JFrame(header) {
         buttons.layout = GridLayout(1, 2)
 
         val cancel = JButton(CANCEL)
-        cancel.addActionListener {
-            this.dispose()
-        }
+        cancel.addActionListener { this.dispose() }
         buttons.add(cancel)
 
         val submit = JButton(SUBMIT)
-        submit.addActionListener {
+        submit.addActionListener(Connection(this))
+        buttons.add(submit)
+
+        this.getRootPane().defaultButton = submit
+        mainContainer.add(buttons, createGridBagConstraints(0, 2, 1.0, 0.0, 2))
+    }
+
+
+    private class AnonymousSelection(private val parent: FTPSettingsWindow) : ItemListener {
+        override fun itemStateChanged(it: ItemEvent) {
+            when (it.stateChange) {
+                ItemEvent.SELECTED -> {
+                    parent.userLabel.isEnabled = false
+                    parent.user.isEnabled = false
+                    parent.passwordLabel.isEnabled = false
+                    parent.password.isEnabled = false
+
+                    parent.user.text = "anonymous"
+                    parent.password.text = ""
+                }
+                ItemEvent.DESELECTED -> {
+                    parent.userLabel.isEnabled = true
+                    parent.user.isEnabled = true
+                    parent.passwordLabel.isEnabled = true
+                    parent.password.isEnabled = true
+
+                    parent.user.text = ""
+                }
+            }
+        }
+    }
+
+    private class Connection(private val parent: FTPSettingsWindow) : ActionListener {
+        override fun actionPerformed(p0: ActionEvent?) {
+
             val client = FTPClient()
 
-            if (!address.text.startsWith(FTP_PREFIX))
-                address.text = FTP_PREFIX + address.text
+            if (!parent.address.text.startsWith(FTP_PREFIX))
+                parent.address.text = FTP_PREFIX + parent.address.text
 
             try {
-                client.connect(address.text.substringAfter(FTP_PREFIX))
+                // sometimes it is really time consuming (I guess client.login is the same)
+                // should be done in other thread
+                client.connect(parent.address.text.substringAfter(FTP_PREFIX))
             } catch (e: ConnectException) {
-                error.reloadText("Unable to connect to the server")
-                return@addActionListener
+                parent.error.reloadText("Unable to connect to the server")
+                return
             } catch (e: Exception) {
-                error.reloadText(e.localizedMessage)
-                return@addActionListener
+                parent.error.reloadText(e.localizedMessage)
+                return
             }
 
             // https://stackoverflow.com/questions/10443308/why-gettext-in-jpasswordfield-was-deprecated
             // Using this library, can't get rid of this insecure line, sorry
-            if (!client.login(user.text, password.text)) {
-                error.reloadText("Invalid credentials. Please try again")
-                return@addActionListener
+            if (!client.login(parent.user.text, parent.password.text)) {
+                parent.error.reloadText(
+                    if (parent.anonymous.isSelected) "Unable to connect as anonymous"
+                    else "Invalid credentials. Please try again"
+                )
+                return
             }
 
             client.setFileType(FTP.BINARY_FILE_TYPE)
@@ -89,13 +134,9 @@ class FTPSettingsWindow(header: String, parent: JFrame) : JFrame(header) {
                 }
             })
 
-            this.dispose()
             parent.dispose()
+            parent.parent.dispose()
             app.isVisible = true
         }
-        buttons.add(submit)
-
-        this.getRootPane().defaultButton = submit
-        mainContainer.add(buttons, createGridBagConstraints(0, 2, 1.0, 0.0, 2))
     }
 }
